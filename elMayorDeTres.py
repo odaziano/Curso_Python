24720412b8681848702090cd325cb1c6e7ef5416
odaziano <138295974+odaziano@users.noreply.github.com>
2023-10-30 20:47:21 -0300

    Eliminar archivo elMayorDetres.py

--- elMayorDeTres.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input("Ingrese n1: "))
-n2 = int(input("Ingrese n2: "))
-n3 = int(input("Ingrese n3: "))
-if n1 > n2 and n1 > n3:
-    print("El mayor es N1: "+str(n1))
-elif n2 > n3:
-    print("El mayor es N2: "+str(n2))
-else:
-    print("El mayor es N3: "+str(n3))
