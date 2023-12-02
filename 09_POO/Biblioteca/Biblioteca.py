d71bccea6ca1b99eb129da9ce6ae359a2efc5d05
odaziano <138295974+odaziano@users.noreply.github.com>
2023-12-02 18:23:09 -0300

    Add files via upload

---
+++ 09_POO/Biblioteca/Biblioteca.py
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+"""### **Sistema de Biblioteca**
+
+Crea una clase llamada **`Libro`** con los siguientes atributos:
+
+- **`titulo`**
+- **`autor`**
+- **`año_publicacion`**
+- **`disponible`** (inicializado en True)
+- `**unidades**`
+
+Crea otra clase llamada **`Biblioteca`** que tenga una lista de libros disponibles. Implementa métodos para:
+
+- Mostrar todos los libros disponibles.
+- Prestar un libro (cambia el estado de **`disponible`** a False).
+- Devolver un libro (cambia el estado de **`disponible`** a True).
+
+=======
+"""### **Sistema de Biblioteca**
+
+Crea una clase llamada **`Libro`** con los siguientes atributos:
+
+- **`titulo`**
+- **`autor`**
+- **`año_publicacion`**
+- **`disponible`** (inicializado en True)
+- `**unidades**`
+
+Crea otra clase llamada **`Biblioteca`** que tenga una lista de libros disponibles. Implementa métodos para:
+
+- Mostrar todos los libros disponibles.
+- Prestar un libro (cambia el estado de **`disponible`** a False).
+- Devolver un libro (cambia el estado de **`disponible`** a True).
+
+>>>>>>> faed99a8dc5c033e9e84224320f0d59e22ae1a64
+Por último, crear un sistema que permita seleccionar entre bibliotecas (crear al menos 2) y agregar o quitar libros de las mismas. El sistema debe permitir guardar la base de libros de la biblioteca en un archivo **JSON**, o crear nuevas bibliotecas a partir de un archivo ********JSON********."""
